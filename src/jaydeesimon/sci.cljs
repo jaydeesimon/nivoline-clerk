6cc8e329752ce11f83f70d89dd5afbfed5c9d93d
Jeffrey Simon <jdsimon@gmail.com>
2023-02-02 20:13:06 -0500

    working example with @nivo/line

--- src/jaydeesimon/sci.cljs
+++ src/jaydeesimon/sci.cljs
@@ -1,12 +1,13 @@
 (ns jaydeesimon.sci
-  (:require [jaydeesimon.viewer]
-            [nextjournal.clerk.sci-env]
+  (:require [nextjournal.clerk.sci-env]
             [sci.ctx-store]
-            [sci.core :as sci]))
+            [sci.core :as sci]
+            ["@nivo/line" :refer [ResponsiveLine]]))
 
 (sci.ctx-store/swap-ctx!
   sci/merge-opts
-  {:aliases    {'jdsv 'jaydeesimon.viewer}
+  {:classes {'ResponsiveLine ResponsiveLine}
+   :aliases    {'jdsv 'jaydeesimon.viewer}
    :namespaces
    {'jaydeesimon.viewer
     (sci/copy-ns jaydeesimon.viewer (sci/create-ns 'jaydeesimon.viewer))}})
